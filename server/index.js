a779108f23846e9d7cf146524dc48e40be6c1bf5
TanushaVeraksa <veraksa161@gmail.com>
2023-08-15 23:03:55 +0300

    added vercel

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const sequelize = require("./db");
+const models = require("./models/model");
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorMiddleware')
 const cors = require("cors");
@@ -20,7 +21,7 @@ const start = async () => {
         await sequelize.sync();
         app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
     } catch(e){
-       throw Error(e)
+        console.log(e)
     }
 }
 
